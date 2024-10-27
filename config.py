99d3900b528a56327c552da9534a8ec1b51638c1
Rida Syeda <ridasyeda611@gmail.com>
2024-10-27 10:40:24 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 #TODO
 #API key for Google Generative AI 
 api_key="WRITE_HERE_GEMINI_AI_KEY"
-gcs_path_file = "WRITE_HERE_KEY_FILE_PATH"
+gcs_path_file = "WRITE_HERE_JSON_KEY_FILE_PATH"
 
 def setup_gcp():
     # Set the path to the service account key located in the current folder
-    credential_path = os.path.join(os.path.dirname(__file__), "aiatl-439722-78a2f30a5602.json")
+    credential_path = os.path.join(os.path.dirname(__file__), gcs_path_file)
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 
